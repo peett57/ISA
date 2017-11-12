19ac80c4fc9a6f54b1fc331c2085179dc0430761
Peter Revaj <revajpeter57@gmail.com>
2017-11-12 21:43:19 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -188,7 +188,7 @@ int main(int argc, char *argv[]){
 	while(*network){
 		if(isdigit((unsigned char)*network)){
 			value[index] *=10;
-			value[index] +=*str - '0';
+			value[index] += *network - '0';
 		}else{
 			index++;
 		}
