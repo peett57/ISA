14eb50e7ffb678427aaa8ee79ebf062ba4fae86b
Peter Revaj <revajpeter57@gmail.com>
2017-11-13 15:43:36 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -279,63 +279,15 @@ int main(int argc, char *argv[]){
 	cout << "4  " << byte4 << endl;
 	
 
-	int maska_na_cislo;
-	if(mask_int == 1 ){maska_na_cislo = 127;}
-	else if(mask_int == 2 ){maska_na_cislo = 63;}
-	else if(mask_int == 3 ){maska_na_cislo = 31;}
-	else if(mask_int == 4 ){maska_na_cislo = 15;}
-	else if(mask_int == 5 ){maska_na_cislo = 7;}
-	else if(mask_int == 6 ){maska_na_cislo = 3;}
-	else if(mask_int == 7 ){maska_na_cislo = 1;}
-	else if(mask_int == 8 ){maska_na_cislo = 0;}
-	else{
-		fprintf((stderr), "Wrong Mask\n");
-		return 1;
-	}
-
-
-	int mask_byte;
-	int byte1_end, byte2_end, byte3_end, byte4_end;
 	
-	if(mask_int <= 8){
-		mask_byte = 1;
-		byte1_end = byte1 - maska_na_cislo;
-		byte2 = 0;
-		byte3 = 0;
-		byte4 = 0;
-	}
-	else if(mask_int <= 16){
-		mask_byte = 2;
-		mask_int = mask_int - 8;
-		byte2_end = byte2 - maska_na_cislo;
-		byte3 = 0;
-		byte4 = 0;
 
-	}
-	else if(mask_int <= 24){
-		mask_byte = 3;
-		mask_int = mask_int - 16;
-		byte3_end = byte3 - maska_na_cislo;
-		byte4 = 0;
-	}
-	else{
-		mask_byte = 4;
-		mask_int = mask_int - 24;
-		byte4_end = byte4 - maska_na_cislo;
-	}
 
-	cout << "mask byte : " << mask_byte << endl;
-	cout << "1  " << byte1 << endl;
-	cout << "2  " << byte2 << endl;
-	cout << "3  " << byte3 << endl;
-	cout << "4  " << byte4 << endl;
+	
 
 	
 	
 
-	for(int i = byte1_end; i <= byte1 ; i++){
-		cout << i << endl;
-	}
+	
 
 
 
