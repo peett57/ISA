d04a6cb3d01d56815c87001f2cdb88ad50dd689b
Peter Revaj <revajpeter57@gmail.com>
2017-11-14 12:20:41 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -530,7 +530,9 @@ int main(int argc, char *argv[]){
 
 		serv_addr.sin_port = htons(portno);
 
-		if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) >= 0){
+		if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+			cout <<  " not connected "  << endl;
+		}else{
 			cout << char_ip_for_scan << " TCP " << portno << endl;
 		}
 
