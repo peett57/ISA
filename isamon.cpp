8016a798e4b837d817d51bf707155a7e3fff11bc
Peter Revaj <revajpeter57@gmail.com>
2017-11-14 13:06:28 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -498,7 +498,7 @@ int main(int argc, char *argv[]){
 
 	if(argumenty.wait > 0){
 			struct timeval timeout;
-			timeout.tv_sec = 0.001*atoi(argumenty.wait);
+			timeout.tv_sec = 0.001*argumenty.wait;
     		timeout.tv_usec = 0;
     		cout << timeout.tv_sec << endl;	
 	}
