1e36f188ccc145967149ed9bbd5bd273bc99b319
Peter Revaj <revajpeter57@gmail.com>
2017-11-14 11:53:12 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -452,9 +452,9 @@ int main(int argc, char *argv[]){
 
 					//cout << "IP address - char: " << char_ip_for_scan << endl;
 
-					for(int x = 1 ; x <= 200; x++){
-						int portno = x;
-						const char *hostname = char_ip_for_scan;
+					/*for(int x = 1 ; x <= 1; x++){
+						int portno = 22;
+						const char *hostname = "10.190.22.160";
 
 						int sockfd;
 						struct sockaddr_in serv_addr;
@@ -485,7 +485,7 @@ int main(int argc, char *argv[]){
 
 						close(sockfd);
 
-					}
+					}*/
 
 
 
@@ -500,6 +500,42 @@ int main(int argc, char *argv[]){
 				}
 			}
 		}
+	}
+
+
+	for(int x = 1 ; x <= 1; x++){
+		int portno = 22;
+		const char *hostname = "10.190.22.160";
+
+		int sockfd;
+		struct sockaddr_in serv_addr;
+		struct hostent *server;
+
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if(sockfd < 0 ){
+			fprintf((stderr), "socket: %d.%d.%d.%d - %d\n" , i,j,k,l,x);
+			return 1;
+		}
+
+		server = gethostbyname(hostname);
+		if(server == NULL){
+			fprintf((stderr), "gethostbyname: %d.%d.%d.%d - %d\n" , i,j,k,l,x);
+			return 1;
+		}
+
+		bzero((char *) &serv_addr, sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;
+		bcopy((char * )&server->h_addr,
+			(char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+		serv_addr.sin_port = htons(portno);
+
+		if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) >= 0){
+			cout << char_ip_for_scan << " TCP " << portno << endl;
+		}
+
+		close(sockfd);
+
 	}
 
 	return 0;
