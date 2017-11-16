afb5de626749fa4adb7deb9d142ff817b66103a8
Peter Revaj <revajpeter57@gmail.com>
2017-11-16 21:00:26 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -609,6 +609,7 @@ int main(int argc, char *argv[]){
 			       	//prijatie odpovedi
 			       	while(1){
 			       		length = recvfrom(sd, buffer, BUF_SIZE, 0, NULL, NULL);
+			       		cout << "length : " << length << endl;
 		                if (length == -1){
 		                    fprintf((stderr), "receive:  %d.%d.%d.%d\n", i,j,k,l );
 							return 1;
