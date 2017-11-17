c7e802cf77ad8708578759a760d7019302038f97
Peter Revaj <revajpeter57@gmail.com>
2017-11-17 10:22:26 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -559,220 +559,226 @@ int main(int argc, char *argv[]){
     		timeout.tv_usec = (argumenty.wait % 1000) * 1000;	
 	}
 
-	stringstream convert;
-	string str_ip_for_scan;
-	const char * char_ip_for_scan;
-	for(int i = final1 ; i <= end1 ; i++){
-		for(int j = final2; j <= end2; j++){
-			for(int k = final3; k <= end3; k++){
-				for(int l = final4; l <= end4 ; l++){
-					//cout << "IP address: " << i << "." << j << "." << k << "." << l << endl; 
-					//vsetky ip adresy ktore mam prechadzat na danom networku 
-					convert << i;
-					str_ip_for_scan += convert.str();
-					convert.str("");
+	bool vypnutie == true;
+	if(vypnutie == false){
+		stringstream convert;
+		string str_ip_for_scan;
+		const char * char_ip_for_scan;
+		for(int i = final1 ; i <= end1 ; i++){
+			for(int j = final2; j <= end2; j++){
+				for(int k = final3; k <= end3; k++){
+					for(int l = final4; l <= end4 ; l++){
+						//cout << "IP address: " << i << "." << j << "." << k << "." << l << endl; 
+						//vsetky ip adresy ktore mam prechadzat na danom networku 
+						convert << i;
+						str_ip_for_scan += convert.str();
+						convert.str("");
 
-					str_ip_for_scan += '.';
+						str_ip_for_scan += '.';
 
-					convert << j;
-					str_ip_for_scan += convert.str();
-					convert.str("");
+						convert << j;
+						str_ip_for_scan += convert.str();
+						convert.str("");
 
-					str_ip_for_scan += '.';
+						str_ip_for_scan += '.';
 
-					convert << k;
-					str_ip_for_scan += convert.str();
-					convert.str("");
+						convert << k;
+						str_ip_for_scan += convert.str();
+						convert.str("");
 
-					str_ip_for_scan += '.';
+						str_ip_for_scan += '.';
 
-					convert << l;
-					str_ip_for_scan += convert.str();
-					convert.str("");
+						convert << l;
+						str_ip_for_scan += convert.str();
+						convert.str("");
 
-					//
-					char_ip_for_scan = str_ip_for_scan.c_str();
+						//
+						char_ip_for_scan = str_ip_for_scan.c_str();
 
-					//cout << "IP address - char: " << char_ip_for_scan << endl;
+						//cout << "IP address - char: " << char_ip_for_scan << endl;
 
-					/*if(i == 10 && j == 190 && k == 23 && l == 253 ){
-						break;
-					}*/	
+						/*if(i == 10 && j == 190 && k == 23 && l == 253 ){
+							break;
+						}*/	
 
-					int sd;
-					unsigned char buffer[BUF_SIZE];
-					unsigned char source_ip[4] = {10,190,23,178};
-					unsigned char target_ip[4] = {i,j,k,l};
-					struct ifreq ifr;
-					struct ethhdr *send_req = (struct ethhdr *)buffer;
-					struct ethhdr *rcv_resp= (struct ethhdr *)buffer;
-					struct arp_header *arp_req = (struct arp_header *)(buffer+ETH2_HEADER_LEN);
-					struct arp_header *arp_resp = (struct arp_header *)(buffer+ETH2_HEADER_LEN);
-					struct sockaddr_ll socket_address;
-					int ret,length=0,ifindex;
+						int sd;
+						unsigned char buffer[BUF_SIZE];
+						unsigned char source_ip[4] = {10,190,23,178};
+						unsigned char target_ip[4] = {i,j,k,l};
+						struct ifreq ifr;
+						struct ethhdr *send_req = (struct ethhdr *)buffer;
+						struct ethhdr *rcv_resp= (struct ethhdr *)buffer;
+						struct arp_header *arp_req = (struct arp_header *)(buffer+ETH2_HEADER_LEN);
+						struct arp_header *arp_resp = (struct arp_header *)(buffer+ETH2_HEADER_LEN);
+						struct sockaddr_ll socket_address;
+						int ret,length=0,ifindex;
 
-					memset(buffer,0x00,60);
+						memset(buffer,0x00,60);
 
-					//open socket
-					sd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-					if (sd == -1) {
-                		fprintf((stderr), "socket:  - %d.%d.%d.%d\n", i,j,k,l );
-						return 1;
-        			}
+						//open socket
+						sd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+						if (sd == -1) {
+	                		fprintf((stderr), "socket:  - %d.%d.%d.%d\n", i,j,k,l );
+							return 1;
+	        			}
 
-        			//bude sa robit na eth1
-        			strcpy(ifr.ifr_name,"eth1");
+	        			//bude sa robit na eth1
+	        			strcpy(ifr.ifr_name,"eth1");
 
-        			//ethernet index
-        			if (ioctl(sd, SIOCGIFINDEX, &ifr) == -1) {
-       					fprintf((stderr), "SIOCGIFINDEX  - \n" );
-						return 1;
-    				}
-    				ifindex = ifr.ifr_ifindex;
+	        			//ethernet index
+	        			if (ioctl(sd, SIOCGIFINDEX, &ifr) == -1) {
+	       					fprintf((stderr), "SIOCGIFINDEX  - \n" );
+							return 1;
+	    				}
+	    				ifindex = ifr.ifr_ifindex;
 
-    				// ziskanie MAC
-        			if (ioctl(sd, SIOCGIFHWADDR, &ifr) == -1) {
-                		perror("SIOCGIFINDEX");
-                		exit(1);
-        			}
+	    				// ziskanie MAC
+	        			if (ioctl(sd, SIOCGIFHWADDR, &ifr) == -1) {
+	                		perror("SIOCGIFINDEX");
+	                		exit(1);
+	        			}
 
-        			close(sd);
+	        			close(sd);
 
-        			for (int index = 0; index < 6; index++){
-        				send_req->h_dest[index] = (unsigned char)0xff;
-        				arp_req->target_mac[index] = (unsigned char)0x00;
-        				// doplnenie source MAC do hlavicky
-        				send_req->h_source[index] = (unsigned char)ifr.ifr_hwaddr.sa_data[index];
-                		arp_req->sender_mac[index] = (unsigned char)ifr.ifr_hwaddr.sa_data[index];
-                		socket_address.sll_addr[index] = (unsigned char)ifr.ifr_hwaddr.sa_data[index];
+	        			for (int index = 0; index < 6; index++){
+	        				send_req->h_dest[index] = (unsigned char)0xff;
+	        				arp_req->target_mac[index] = (unsigned char)0x00;
+	        				// doplnenie source MAC do hlavicky
+	        				send_req->h_source[index] = (unsigned char)ifr.ifr_hwaddr.sa_data[index];
+	                		arp_req->sender_mac[index] = (unsigned char)ifr.ifr_hwaddr.sa_data[index];
+	                		socket_address.sll_addr[index] = (unsigned char)ifr.ifr_hwaddr.sa_data[index];
 
-        			}
-
-
-        			//priprava sockaddr_ll
-        			socket_address.sll_family = AF_PACKET;
-			        socket_address.sll_protocol = htons(ETH_P_ARP);
-			        socket_address.sll_ifindex = ifindex;
-			        socket_address.sll_hatype = htons(ARPHRD_ETHER);
-			        socket_address.sll_pkttype = (PACKET_BROADCAST);
-			        socket_address.sll_halen = MAC_LENGTH;
-			        socket_address.sll_addr[6] = 0x00;
-			        socket_address.sll_addr[7] = 0x00;
-
-			        // protocol pre packet
-			        send_req->h_proto = htons(ETH_P_ARP);
-
-			        //vytvorenie arp requestu
-			        arp_req->hardware_type = htons(HW_TYPE);
-			        arp_req->protocol_type = htons(ETH_P_IP);
-			        arp_req->hardware_len = MAC_LENGTH;
-			        arp_req->protocol_len =IPV4_LENGTH;
-			        arp_req->opcode = htons(ARP_REQUEST);
-			        for(int index=0;index<5;index++)
-			        {
-			                arp_req->sender_ip[index]=(unsigned char)source_ip[index];
-			                arp_req->target_ip[index]=(unsigned char)target_ip[index];
-			        }
+	        			}
 
 
+	        			//priprava sockaddr_ll
+	        			socket_address.sll_family = AF_PACKET;
+				        socket_address.sll_protocol = htons(ETH_P_ARP);
+				        socket_address.sll_ifindex = ifindex;
+				        socket_address.sll_hatype = htons(ARPHRD_ETHER);
+				        socket_address.sll_pkttype = (PACKET_BROADCAST);
+				        socket_address.sll_halen = MAC_LENGTH;
+				        socket_address.sll_addr[6] = 0x00;
+				        socket_address.sll_addr[7] = 0x00;
+
+				        // protocol pre packet
+				        send_req->h_proto = htons(ETH_P_ARP);
+
+				        //vytvorenie arp requestu
+				        arp_req->hardware_type = htons(HW_TYPE);
+				        arp_req->protocol_type = htons(ETH_P_IP);
+				        arp_req->hardware_len = MAC_LENGTH;
+				        arp_req->protocol_len =IPV4_LENGTH;
+				        arp_req->opcode = htons(ARP_REQUEST);
+				        for(int index=0;index<5;index++)
+				        {
+				                arp_req->sender_ip[index]=(unsigned char)source_ip[index];
+				                arp_req->target_ip[index]=(unsigned char)target_ip[index];
+				        }
 
 
-			        // vytvorenie raw socketu na request
-			        if ((sd = socket (PF_PACKET, SOCK_RAW, htons (ETH_P_ALL))) < 0) {
-					    fprintf((stderr), "socket:  %d.%d.%d.%d\n", i,j,k,l );
-						return 1;
+
+
+				        // vytvorenie raw socketu na request
+				        if ((sd = socket (PF_PACKET, SOCK_RAW, htons (ETH_P_ALL))) < 0) {
+						    fprintf((stderr), "socket:  %d.%d.%d.%d\n", i,j,k,l );
+							return 1;
+						}
+
+
+						/*if(argumenty.wait > 0){
+							if (setsockopt (sd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0){
+								fprintf((stderr), "setsockopt:  \n" );
+								return 1;
+							}
+							if (setsockopt (sd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout)) < 0){
+								fprintf((stderr), "setsockopt:  \n" );
+								return 1;
+							}
+						}*/
+						
+
+
+
+
+						buffer[32] = 0x00;
+
+						//odoslanie arp requestu 
+						ret = sendto(sd, buffer, 42, 0, (struct  sockaddr*)&socket_address, sizeof(socket_address));
+				        if (ret == -1)
+				        {
+				            fprintf((stderr), "sendto:  %d.%d.%d.%d\n", i,j,k,l );
+							return 1;
+				        }
+
+				       	memset(buffer,0x00,60);
+
+				       	//prijatie odpovedi
+				       	while(1){
+
+
+				       		length = recvfrom(sd, buffer, BUF_SIZE, 0, NULL, NULL);
+
+				       		
+
+			                if (length == -1){
+			                    fprintf((stderr), "receive:  %d.%d.%d.%d\n", i,j,k,l );
+								return 1;
+			                }
+			                if(htons(rcv_resp->h_proto) == PROTO_ARP){
+
+			                	if(arp_resp->sender_ip[0] == i){
+			                		if(arp_resp->sender_ip[1] == j){
+			                			if(arp_resp->sender_ip[2] == k){
+			                				if(arp_resp->sender_ip[3] == l){
+			                					cout << char_ip_for_scan << endl;
+			                					if(argumenty.t == true){
+			                						if(tpc_check(char_ip_for_scan,argumenty.port,argumenty.wait) != 0){
+			                							fprintf((stderr), "TCP  \n" );
+														return 1;
+			                						}
+			                					}
+			                				}
+			                			}
+			                		}
+			                	}
+			                	if(10 == i){
+			                		if(190 == j){
+			                			if(23 == k){
+			                				if(178 == l){
+			                					cout << char_ip_for_scan << endl;
+			                					if(argumenty.t == true){
+			                						if(tpc_check(char_ip_for_scan,argumenty.port,argumenty.wait) != 0){
+			                							fprintf((stderr), "TCP  \n" );
+														return 1;
+			                						}
+			                					}
+			                				}
+			                			}
+			                		}
+			                	}
+			                	break;
+			                }
+
+			                
+
+				       	}
+
+
+				       	close(sd);
+						//vymazanie a priprava na dalsiu IP
+						str_ip_for_scan = "";
+						char_ip_for_scan = str_ip_for_scan.c_str();
+
+
+						
 					}
-
-
-					/*if(argumenty.wait > 0){
-						if (setsockopt (sd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0){
-							fprintf((stderr), "setsockopt:  \n" );
-							return 1;
-						}
-						if (setsockopt (sd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout)) < 0){
-							fprintf((stderr), "setsockopt:  \n" );
-							return 1;
-						}
-					}*/
-					
-
-
-
-
-					buffer[32] = 0x00;
-
-					//odoslanie arp requestu 
-					ret = sendto(sd, buffer, 42, 0, (struct  sockaddr*)&socket_address, sizeof(socket_address));
-			        if (ret == -1)
-			        {
-			            fprintf((stderr), "sendto:  %d.%d.%d.%d\n", i,j,k,l );
-						return 1;
-			        }
-
-			       	memset(buffer,0x00,60);
-
-			       	//prijatie odpovedi
-			       	while(1){
-
-
-			       		length = recvfrom(sd, buffer, BUF_SIZE, 0, NULL, NULL);
-
-			       		
-
-		                if (length == -1){
-		                    fprintf((stderr), "receive:  %d.%d.%d.%d\n", i,j,k,l );
-							return 1;
-		                }
-		                if(htons(rcv_resp->h_proto) == PROTO_ARP){
-
-		                	if(arp_resp->sender_ip[0] == i){
-		                		if(arp_resp->sender_ip[1] == j){
-		                			if(arp_resp->sender_ip[2] == k){
-		                				if(arp_resp->sender_ip[3] == l){
-		                					cout << char_ip_for_scan << endl;
-		                					if(argumenty.t == true){
-		                						if(tpc_check(char_ip_for_scan,argumenty.port,argumenty.wait) != 0){
-		                							fprintf((stderr), "TCP  \n" );
-													return 1;
-		                						}
-		                					}
-		                				}
-		                			}
-		                		}
-		                	}
-		                	if(10 == i){
-		                		if(190 == j){
-		                			if(23 == k){
-		                				if(178 == l){
-		                					cout << char_ip_for_scan << endl;
-		                					if(argumenty.t == true){
-		                						if(tpc_check(char_ip_for_scan,argumenty.port,argumenty.wait) != 0){
-		                							fprintf((stderr), "TCP  \n" );
-													return 1;
-		                						}
-		                					}
-		                				}
-		                			}
-		                		}
-		                	}
-		                	break;
-		                }
-
-		                
-
-			       	}
-
-
-			       	close(sd);
-					//vymazanie a priprava na dalsiu IP
-					str_ip_for_scan = "";
-					char_ip_for_scan = str_ip_for_scan.c_str();
-
-
-					
 				}
 			}
 		}
+	}
+	else{
+		cout << "close" << endl; 
 	}
 
 
