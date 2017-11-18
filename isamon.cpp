4fe37b991fa9d9ba250c8e31c9a995cb0b277b45
Peter Revaj <revajpeter57@gmail.com>
2017-11-18 20:13:44 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -327,7 +327,7 @@ int udp_check(const char * ip, long int port_arg, long int wait){
 
     		struct icmp *icmp = (struct icmp *)(buffer + iplen);
     		//cout <<"port : " << x << " " << icmp->icmp_code << endl;
-    		cout << x << " pred kontrolou" << endl; 
+    		
     		if((icmp->icmp_type == ICMP_UNREACH) && (icmp->icmp_code == ICMP_UNREACH_PORT)){
     			cout << x << " Unreachable" << endl; 
     			break;              
