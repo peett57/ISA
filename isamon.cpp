b15ffbc09b33938eadc6002d34f7b5a8c8ab1a33
Peter Revaj <revajpeter57@gmail.com>
2017-11-18 17:42:33 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -275,7 +275,7 @@ int udp_check(const char * ip, long int port_arg, long int wait){
 				timeout.tv_sec = wait /1000;
 	    		timeout.tv_usec = (wait % 1000) * 1000;	
 
-	    		cout << timeout.tv_sec << " - " << timeout.tv_usec << endl;
+	    		//cout << timeout.tv_sec << " - " << timeout.tv_usec << endl;
 		}
 		else{
 			fprintf((stderr), "pri UDP musi byt wait:   \n");
