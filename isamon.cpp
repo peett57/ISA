51155199920c5d2b4ef4af0e296c08a8cd2b72ef
Peter Revaj <revajpeter57@gmail.com>
2017-11-18 16:36:29 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -308,10 +308,10 @@ int udp_check(const char * ip, long int port_arg, long int wait){
    				fprintf((stderr), "select -1:  %d\n", x );
 				return 1;
    			}
-   			else if(rv == 0){
+   			else if(!FD_ISSET(recvsd, &fds)){
 
    				//fprintf((stderr), "timeout:  %d.%d.%d.%d\n", i,j,k,l );
-   				cout <<"port : " << x << " timeout "  << endl;
+   				cout << ip << " UDP " << x << endl;
    				
    				break;
 
@@ -333,9 +333,7 @@ int udp_check(const char * ip, long int port_arg, long int wait){
 
 	    		if((icmp->icmp_type == ICMP_UNREACH) && (icmp->icmp_code == ICMP_UNREACH_PORT)){
 	    			break;
-	    		}else{
-	    			cout << ip << " UDP " << x << endl;
-	    		}
+	    		
                 
             
    			}
@@ -1030,10 +1028,10 @@ int main(int argc, char *argv[]){
 		}
 	}
 	else{
-		//udp_check("10.190.22.250",argumenty.port,argumenty.wait); 
+		udp_check("10.190.22.250",argumenty.port,argumenty.wait); 
 		//udp_check("127.0.0.1",argumenty.port,argumenty.wait); 
 		//tcp_check("10.0.2.3",argumenty.port,argumenty.wait); 
-		cout << "closed" << endl;
+		//cout << "closed" << endl;
 	}
 
 
