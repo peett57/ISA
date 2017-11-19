858aedd66a1d414d1f04fb0268051874ce6707a8
Peter Revaj <revajpeter57@gmail.com>
2017-11-19 09:39:33 +0100

    Add files via upload

--- isamon.cpp
+++ isamon.cpp
@@ -270,7 +270,7 @@ int udp_check(const char * ip, long int port_arg, long int wait){
 			return 1;
 		}
 
-
+		memset(buffer,0x00,60);
 		struct timeval timeout;
 		if(wait > 0){
 				
@@ -284,7 +284,7 @@ int udp_check(const char * ip, long int port_arg, long int wait){
 			return 1;
 		}
 
-		memset(buffer,0x00,60);
+		
 		//fcntl(recvsd, F_SETFL, O_NONBLOCK); 
 
 
